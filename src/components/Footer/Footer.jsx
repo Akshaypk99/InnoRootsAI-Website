d07d28bccef762b06bfacda2ee487246cb684a00
akshay-zog <akshay.p@zogglobal.com>
2025-02-24 14:10:06 +0000

    mobile layout for home

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,18 +7,42 @@ const Footer = () => {
 
       </div>
       <ul className='menu-options'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Courses</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li>
+          Home
+          <img src='/images/icons/menu-arrow.svg' />
+        </li>
+        <li>
+          About
+          <img src='/images/icons/menu-arrow.svg' />
+        </li>
+        <li>
+          Courses
+          <img src='/images/icons/menu-arrow.svg' />
+        </li>
+        <li className='seperator'>
+          Contact Us
+          <img src='/images/icons/menu-arrow.svg' />
+        </li>
+
+        <li>
+          Privacy Policy
+          <img src='/images/icons/menu-arrow.svg' />
+        </li>
+        <li>
+          Terms & Conditions
+          <img src='/images/icons/menu-arrow.svg' />
+        </li>
       </ul>
       <div className='sm-btns'>
         <div className='credits'>
           <div className='icons'>
-            <img src='/images/footer/ig.svg' />
             <img src='/images/footer/linkedin.svg' />
             <img src='/images/footer/fb.svg' />
+            <img src='/images/footer/ig.svg' />
+
+            <img src='/images/footer/sm-4.svg' />
+            <img src='/images/footer/sm-5.svg' />
+
             <img src='/images/footer/x.svg' />
             <img src='/images/footer/yt.svg' />
           </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,50 @@
-import React from 'react'
-import './NavBar.scss'
-import logo from '../../assets/logo.svg'
+import React, { useState, useEffect } from "react";
+import "./NavBar.scss";
+import logo from "../../assets/logo.svg";
+import { FaBars, FaTimes } from "react-icons/fa"; // Import icons for hamburger menu
 
 const NavBar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='navbar'>
-      <div className='navbar-left'>
+    <div className={`navbar ${scrolled ? "scrolled" : ""}`}>
+      <div className="navbar-left">
         {/* <img className='' src={logo} /> */}
-        {/* <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
-        </ul> */}
       </div>
-      <div className='navbar-right'>
+
+      {/* Hamburger Menu Button */}
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* Nav Links */}
+      <div className={`navbar-right ${menuOpen ? "open" : ""}`}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Courses</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <li onClick={() => setMenuOpen(false)}>Home</li>
+          <li onClick={() => setMenuOpen(false)}>About</li>
+          <li onClick={() => setMenuOpen(false)}>Courses</li>
+          <li onClick={() => setMenuOpen(false)}>Projects</li>
+          <li onClick={() => setMenuOpen(false)}>Contact</li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
